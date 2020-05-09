25e30b5b4e251f66219e46bced578b910fc63649
Ak Hasanuzzaman <ahasanuzzaman@pdinet.com>
2020-05-09 18:33:26 +0200

    added a gitignore file

---
+++ CovarianceandContravariance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CovarianceandContravariance
+{
+
+    public class Animal
+    {
+        public void Eat() => Console.WriteLine("Eat");
+    }
+    public class Bird : Animal
+    {
+        public void Fly() => Console.WriteLine("Fly");
+    }
+
+    public delegate Animal GetAnimalDelegate();
+    public delegate Bird GetBirdDelegate();
+
+    public delegate void TakeAnimalDelegate(Animal a);
+    public delegate void TakeBirdDelegate(Bird b);
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static Animal GetAnimal() => new Animal();
+            static Bird GetBird() => new Bird();
+
+            static void Eat(Animal animal) => animal.Eat();
+            static void Fly(Bird bird) => bird.Fly();
+
+            GetAnimalDelegate del = GetBird;
+            del();
+
+            TakeBirdDelegate del1 = Eat;
+
+
+            Console.ReadLine();
+        }
+    }
+}
